13c878a3d0ed3ac882eb9f5cf7088c5c085aec89
VadimCanPlayOfficial <141765567+VadimCanPlay@users.noreply.github.com>
2024-09-29 23:40:28 +0300

    Create SpaceHub.lua

---
+++ SpaceHub.lua
@@ -0,0 +1,2 @@
+--Official GitHub link to latest: https://github.com/Lucasfin000/SpaceHub
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Lucasfin000/Orbit-UI-Library/main/Library", true))()
