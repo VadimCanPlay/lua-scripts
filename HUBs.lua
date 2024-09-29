0dbfeb44881f2df55ac902976331ff04520ebb01
VadimCanPlayOfficial <141765567+VadimCanPlay@users.noreply.github.com>
2024-09-29 23:37:41 +0300

    plz read extended
    these are aimed for JJSploit mainly but eclipse hub doesnt work sadly ;<

---
+++ HUBs.lua
@@ -0,0 +1,7 @@
+--Owl Hub:
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+--Dark Hub:
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/Init", true))()
+--Eclipse Hub:
+--getgenv().mainKey = "nil"
+--local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
---
+++ MULTI-SCRIPT.lua
@@ -0,0 +1,8 @@
+--All-in-one file that you NEED
+--this loads all useful scripts btw
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua'),true))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub/main/FE%20Trolling%20GUI.luau"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+loadstring(game:HttpGet("https://pastebin.com/raw/jE85VHx8", true))();
+loadstring(game:HttpGet('https://sirius.menu/script'))();
